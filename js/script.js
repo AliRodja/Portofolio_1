f33fdd791090028a8ed5fbeac1b19c3101ad3cb9
AliRodja <rodjaali@gmail.com>
2025-02-07 11:53:02 +0700

    Feat : Menambahkan Sosmed, dan menambahkan JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var typed = new Typed(".typing", {
+        strings: ["Web Design", "Student", "Junior Developer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+});
